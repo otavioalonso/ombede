fe8a6d20c4cd002761b166368dfb1fb44ce76393
Otávio Alves <oalves@protonmail.com>
2026-01-10 15:10:07 -0300

    create a different log every run

--- server/main.js
+++ server/main.js
@@ -21,7 +21,7 @@ const connection = new Connection({
     signals: ['rpm', 'speed', 'batteryCharge', 'fuelLevel', 'fuelConsumption', 'odometer', 'steeringAngle'],
     calculator: new KaCalculator(),
     debug: false,
-    logFile: './can.log'
+    logFile: `./can-${new Date().toISOString().replace(/[:.]/g, '-')}.log`
 });
 
 connection.connect();
